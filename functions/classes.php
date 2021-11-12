8830de4ce117b550fe884139338920ef5acdcdcc
Antti Koskinen <koskinenaa@users.noreply.github.com>
2021-11-12 15:39:36 +0200

    add body class helper functions

--- functions/classes.php
+++ functions/classes.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+  * Body
+  */
+function helsinki_append_body_classes( array $body, $class ) {
+	if ( is_array( $class ) ) {
+		return array_merge( $body, $class );
+	} else {
+		$body[] = (string) $class;
+		return $body;
+	}
+}
+
+function helsinki_add_body_class_has_n( array $body, string $class ) {
+	return helsinki_append_body_classes( $body, 'has-' . $class );
+}
+
+/**
+  * Element
+  */
 function helsinki_element_classes( string $type, array $classes = array() ) {
   $classes = apply_filters(
 	"helsinki_{$type}_classes",
