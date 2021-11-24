1e1d4f0125b30744628a7e1c6241136dde5d44ba
Antti Koskinen <koskinenaa@users.noreply.github.com>
2021-11-24 13:26:19 +0200

    add gallery lightbox script

--- assets/scripts.js
+++ assets/scripts.js
@@ -88,6 +88,391 @@ function forCallbackLoop(items, callback) {
 	}
 }
 
+function helsinkiGalleryLightbox( config ) {
+
+	'use strict';
+
+	const {strings} = config;
+
+	var currentActive = null;
+
+	/**
+	  * Init
+	  */
+	function initLightboxes( galleries ) {
+		if ( galleries.length === 0 ) {
+			return;
+		}
+
+		for ( let g = 0; g < galleries.length; g++ ) {
+			initLightbox( galleries[g] );
+		}
+	}
+
+	function initLightbox( gallery ) {
+		let figures = galleryFigures( gallery );
+		if ( figures.length === 0 ) {
+			return;
+		}
+
+		if ( ! hasImageLinks( gallery ) ) {
+			return;
+		}
+
+		initGalleryImages( figures );
+
+		let lightbox = createLightbox( gallery, figures );
+		gallery.setAttribute( 'data-lightbox-id', lightbox.id );
+		gallery.after( lightbox );
+
+		lightboxInitialized( gallery, true );
+	}
+
+	function initGalleryImages( figures ) {
+		for ( var f = 0; f < figures.length; f++ ) {
+			initGalleryImage( figures[f] );
+		}
+	}
+
+	function initGalleryImage( figure ) {
+		let imageLink = figure.querySelector( 'a' );
+		imageLink.addEventListener( 'click', clickImage );
+	}
+
+	/**
+	  * Event listeners
+	  */
+	function clickImage( event ) {
+		event.preventDefault();
+		openLightBox(
+			currentGalleryLightBox(
+				currentGallery( event.currentTarget )
+			),
+			event.currentTarget.firstElementChild
+		);
+	}
+
+	function clickClose( event ) {
+		event.preventDefault();
+		closeLightbox( event.target.closest('.lightbox') );
+	}
+
+	function clickNext( event ) {
+		event.preventDefault();
+
+		if ( ! currentActive ) {
+			return;
+		}
+
+		let currentImage = lightboxActiveImage( currentActive );
+		if ( currentImage ) {
+			if ( currentImage.nextElementSibling ) {
+				switchActiveImage( currentImage, currentImage.nextElementSibling );
+			} else {
+				switchActiveImage( currentImage, firstLightboxImage( currentActive ) );
+			}
+		} else {
+			switchActiveImage( null, firstLightboxImage( currentActive ) );
+		}
+	}
+
+	function clickPrevious( event ) {
+		event.preventDefault();
+
+		if ( ! currentActive ) {
+			return;
+		}
+
+		let currentImage = lightboxActiveImage( currentActive );
+		if ( currentImage ) {
+			if ( currentImage.previousElementSibling ) {
+				switchActiveImage( currentImage, currentImage.previousElementSibling );
+			} else {
+				switchActiveImage( currentImage, lastLightboxImage( currentActive ) );
+			}
+		} else {
+			switchActiveImage( null, lastLightboxImage( currentActive ) );
+		}
+	}
+
+	/**
+	  * Interactions
+	  */
+	function openLightBox( lightbox, clickedImage ) {
+		if ( currentActive && currentActive !== lightbox ) {
+			closeLightbox( currentActive );
+		}
+
+		let currentActiveImage = lightboxActiveImage( lightbox ),
+			targetImage = clickedImage ? lightboxFigure( lightbox, clickedImage.src ) : null;
+
+		switchActiveImage( currentActiveImage, targetImage );
+
+		toggleHidden( lightbox, false );
+		toggleActive( lightbox, true );
+		toggleAriaExpanded( lightboxCloseButton( lightbox ), true );
+
+		currentActive = lightbox;
+	}
+
+	function closeLightbox( lightbox ) {
+		toggleHidden( lightbox, true );
+		toggleActive( lightbox, false );
+		toggleAriaExpanded( lightboxCloseButton( lightbox ), false );
+
+		let currentActiveImage = lightboxActiveImage( lightbox );
+		if ( currentActiveImage ) {
+			toggleActive( currentActiveImage, false );
+		}
+
+		currentActive = null;
+	}
+
+	function switchActiveImage( currentImage, newImage ) {
+		if ( currentImage ) {
+			toggleActive( currentImage, false );
+			toggleHidden( currentImage, true );
+		}
+
+		if ( newImage ) {
+			toggleActive( newImage, true );
+			toggleHidden( newImage, false );
+		}
+	}
+
+	/**
+	  * Elements
+	  */
+	function galleryFigures( gallery ) {
+		return gallery.querySelectorAll( '.blocks-gallery-item figure' );
+	}
+
+	function activeLightbox() {
+		return document.querySelector( '.lightbox.is-active' );
+	}
+
+	function lightboxCloseButton( lightbox ) {
+		return lightbox.querySelector( '.lightbox__close' );
+	}
+
+	function lightboxActiveImage( lightbox ) {
+		return lightbox.querySelector( '.lightbox__images .is-active' );
+	}
+
+	function currentGallery( galleryItem ) {
+		return galleryItem.closest( '.wp-block-gallery' );
+	}
+
+	function currentGalleryLightBox( gallery ) {
+		return document.getElementById(
+			gallery.getAttribute( 'data-lightbox-id' )
+		);
+	}
+
+	function firstLightboxImage( lightbox ) {
+		return lightbox.querySelector( '.lightbox__images figure' );
+	}
+
+	function lastLightboxImage( lightbox ) {
+		let figures = lightbox.querySelectorAll( '.lightbox__images figure' );
+		return figures[ figures.length - 1 ];
+	}
+
+	function lightboxFigure( lightbox, source ) {
+		let img = lightbox.querySelector( '.lightbox__images img[src="' + source + '"]' );
+		return img ? img.parentElement : null;
+	}
+
+	/**
+	  * Element creators
+	  */
+	function createLightbox( gallery, figures ) {
+		// Element
+		let lightbox = createContainer( 'div', ['lightbox', 'lightbox--gallery'] );
+		let title = createHeading( 'h2', strings.lightboxTitle );
+
+		// Attributes
+		let lightboxId = generateLightboxId( gallery );
+		lightbox.id = lightboxId;
+
+		title.id = lightboxId + '-title';
+		title.classList.add('screen-reader-text');
+
+		lightbox.setAttribute( 'role', 'dialog' );
+		lightbox.setAttribute( 'aria-modal', 'true' );
+		lightbox.setAttribute( 'aria-labelledby', title.id );
+		toggleHidden( lightbox, true );
+
+		// Children
+		lightbox.append( title );
+		lightbox.append( createImages( figures ) );
+		lightbox.append( createNavigation() );
+		lightbox.append( createCloseButton( lightboxId ) );
+
+		// Output
+		return lightbox;
+	}
+
+	function createContainer( tag, classNames ) {
+		let element = document.createElement(tag);
+		element.classList.add(...classNames);
+		return element;
+	}
+
+	function createHeading( level, text ) {
+		let heading = document.createElement( level );
+		heading.innerHTML = text;
+		return heading;
+	}
+
+	function createCloseButton( lightboxId ) {
+		// Element
+		let button = document.createElement('button');
+
+		// Attributes
+		button.id = 'close-' + lightboxId;
+		button.type = 'button';
+		button.innerHTML = strings.close;
+
+		button.classList.add( 'lightbox__close' );
+
+		button.setAttribute( 'aria-controls', lightboxId );
+		toggleAriaExpanded( button, false );
+
+		// Events
+		button.addEventListener('click', clickClose);
+
+		// Output
+		return button;
+	}
+
+	function createNavigation() {
+		let container = createContainer( 'div', ['lightbox__navigation'] );
+
+		container.append( createNavigationItem( 'prev', strings.prev, clickPrevious ) );
+		container.append( createNavigationItem( 'next', strings.next, clickNext ) );
+
+		return container;
+	}
+
+	function createNavigationItem( type, text, onClick ) {
+		// Element
+		let button = document.createElement('button');
+
+		// Attributes
+		button.type = 'button';
+		button.innerHTML = text;
+		button.classList.add( 'lightbox__' + type );
+
+		// Events
+		button.addEventListener('click', onClick);
+
+		// Output
+		return button;
+	}
+
+	function createImages( figures ) {
+		let container = createContainer( 'div', ['lightbox__images'] );
+
+		for (var f = 0; f < figures.length; f++) {
+			container.append( createImage( figures[f] ) );
+		}
+
+		return container;
+	}
+
+	function createImage( source ) {
+		let figure = document.createElement( 'figure' ),
+			sourceImage = source.querySelector( 'img' ),
+			sourceCaption = source.querySelector( 'figcaption' );
+
+		figure.append( sourceImage.cloneNode() );
+		if ( sourceCaption ) {
+			figure.append( sourceCaption.cloneNode(true) );
+		}
+
+		toggleHidden( figure, true );
+
+		return figure;
+	}
+
+	/**
+	  * Element classes
+	  */
+	function toggleElementClass( element, className, enabled ) {
+		if ( enabled ) {
+			element.classList.add( className );
+		} else {
+			element.classList.remove( className );
+		}
+	}
+
+	function lightboxInitialized( gallery, initialized ) {
+		toggleElementClass( gallery, 'has-lightbox', initialized );
+	}
+
+	function toggleActive( element, active ) {
+		toggleElementClass( element, 'is-active', active );
+	}
+
+	/**
+	  * Aria
+	  */
+	function toggleAriaExpanded( element, expanded ) {
+		if ( expanded ) {
+			element.setAttribute( 'aria-expanded', 'true' );
+		} else {
+			element.setAttribute( 'aria-expanded', 'false' );
+		}
+	}
+
+	function isAriaExpanded( element ) {
+		return 'true' === element.getAttribute( 'aria-expanded' );
+	}
+
+	/**
+	  * Helpers
+	  */
+	function generateLightboxId( gallery ) {
+		if ( gallery.id ) {
+			return 'lightbox-' + gallery.id;
+		}
+		return 'lightbox-' + ( Date.now() + gallery.offsetTop + gallery.clientHeight + gallery.childElementCount );
+	}
+
+	function hasImageLinks( gallery ) {
+		return gallery.querySelector( '.blocks-gallery-item > figure > a' );
+	}
+
+	function toggleHidden( element, hidden ) {
+		if ( hidden ) {
+			element.hidden = true;
+		} else {
+			element.hidden = false;
+		}
+	}
+
+	/**
+	  * Module
+	  */
+	return {
+		init: initLightboxes,
+	};
+}
+
+const HelsinkiGalleryLightbox = helsinkiGalleryLightbox({
+	strings: {
+		close: "Close",
+		next: "Next",
+		prev: "Previous",
+		lightboxTitle: "Gallery images",
+	}
+});
+
+HelsinkiGalleryLightbox.init(
+	document.querySelectorAll( '.wp-block-gallery' )
+);
+
 function jsLoadMoreInit() {
   var loadMores = document.querySelectorAll('.js-load-more');
   if ( loadMores.length > 0 ) {
