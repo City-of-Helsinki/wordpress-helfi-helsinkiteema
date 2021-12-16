e8e27350abc194dc5dd3215c689f6fdf9970c781
Antti Koskinen <koskinenaa@users.noreply.github.com>
2021-12-16 10:34:15 +0200

    remove alignfull support from blocks

--- setup/theme.php
+++ setup/theme.php
@@ -45,8 +45,6 @@ function helsinki_setup_theme()
 	/**
 	 * Block editor
 	 */
-	add_theme_support( 'align-wide' );
-	// custom colors
 	add_theme_support( 'disable-custom-font-sizes' );
 	add_theme_support( 'disable-custom-colors' );
 	add_theme_support( 'disable-custom-gradients' );
