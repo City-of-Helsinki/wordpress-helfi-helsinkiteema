2647f469ea8ce65285c24ed633f4fe44e1adcdf3
Billy Ward <billy@broomudigitals.fi>
2022-03-11 14:53:23 +0200

    feature/WY-97 - hyphenation to finnish headings
    Included Hyphenation.js as a library. Implemented hyphenation for headings globally for the Finnish language.

---
+++ src/js/header/load-hyphenopoly.js
@@ -0,0 +1,16 @@
+Hyphenopoly.config({
+    require: {
+        "fi": "lentokonesuihkuturbiinimoottoriapumekaanikkoaliupseerioppilas",
+        "en-us": "Supercalifragilisticexpialidocious"
+    },
+    setup: {
+        selectors: {
+            "h1": {minWordLength: 14},
+            "h2": {minWordLength: 14},
+            "h3": {minWordLength: 14},
+            "h4": {minWordLength: 14},
+            "h5": {minWordLength: 14},
+            "h6": {minWordLength: 14},
+        }
+    }
+});
