ad46c304adf750bec82dc9890293a1088dc5c108
Billy Ward <billy@broomudigitals.fi>
2022-03-31 10:13:41 +0300

    feature/WY-96 rss-feed accessibility update

--- partials/front-page/feed-posts/list-item.php
+++ partials/front-page/feed-posts/list-item.php
@@ -2,7 +2,9 @@
 	<article class="entry entry--feed entry--post">
 		<a href="<?php echo esc_url( $args['item']->get_permalink() ); ?>">
 			<h3 class="entry__title">
-				<span><?php echo esc_html( helsinki_trim_title( $args['item']->get_title() ) ); ?></span><?php helsinki_svg_icon('link-external'); ?>
+				<span><?php echo esc_html( helsinki_trim_title( $args['item']->get_title() ) ); ?></span>
+				<span class="screen-reader-text"><?php esc_html_e( 'Link forwards to:', 'helsinki-universal' ); ?></span>
+				<?php helsinki_svg_icon('link-external'); ?>
 			</h3>
 		</a>
 		<div class="entry__meta meta">
