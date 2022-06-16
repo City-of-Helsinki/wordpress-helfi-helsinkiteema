e733c6179321ed6f3f58f4275136f19721dcd8b9
Billy Ward <billy@broomudigitals.fi>
2022-06-16 14:25:55 +0300

    feature/WY-168 fix excerpt in RSS-block and include the block title in the table of contents

--- partials/front-page/feed-posts/highlight.php
+++ partials/front-page/feed-posts/highlight.php
@@ -1,8 +1,10 @@
 <?php
 $excerpt = $args['item']->get_description();
 $first_image = helsinki_first_image_from_string($excerpt);
+$first_paragraph = helsinki_first_paragraph_from_string($excerpt);
 if ( $first_image ) {
 	$excerpt = str_replace( $first_image, '', $excerpt );
+	$excerpt = mb_strlen($excerpt) > 400 ? mb_substr($excerpt, 0, 400).'...' : $excerpt;
 }
 ?>
 <article class="entry entry--highlight entry--post">
@@ -14,10 +16,10 @@ if ( $first_image ) {
 			);
 		?>
 		<h3 class="entry__title">
-			<?php echo esc_html( helsinki_trim_title( $args['item']->get_title() ) ); ?>
+			<?php echo esc_html( $args['item']->get_title() ); ?>
 		</h3>
 		<div class="entry__excerpt excerpt size-l show-for-l">
-			<?php echo wp_kses_post( $excerpt ); ?>
+			<?php echo wp_kses_post( !empty($first_paragraph) ? $first_paragraph : $excerpt ); ?>
 		</div>
 		<div class="entry__meta meta">
 			<time class="date" datetime="<?php echo esc_attr( $args['item']->get_date('c') ); ?>">
