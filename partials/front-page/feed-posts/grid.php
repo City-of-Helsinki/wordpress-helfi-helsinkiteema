d0fb276f00bb91c9452794a4facc07b1b31e7824
Billy Ward <billy@broomudigitals.fi>
2022-12-22 08:31:39 +0200

    feature/WY-344 update RSS-block visually and wrap it's a-tags only around link titles

--- partials/front-page/feed-posts/grid.php
+++ partials/front-page/feed-posts/grid.php
@@ -1,18 +1,5 @@
-<div class="grid entries l-up-2">
-	<div class="grid__column">
-		<?php
-			get_template_part(
-				'partials/front-page/feed-posts/highlight',
-				null,
-				array(
-					'item' => array_shift($args['feed_posts']),
-					'date_format' => $args['date_format'],
-					'time_format' => $args['time_format'],
-				)
-			);
-		?>
-	</div>
-	<div class="grid__column">
+<div class="entries l-up-2">
+	<div>
 		<ul class="posts">
 			<?php
 				foreach ($args['feed_posts'] as $feed_item) {
