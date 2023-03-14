a1652ee0c2b56a418ecf25cef10845fb0ab0a9cb
Billy Ward <billy@broomudigitals.fi>
2023-03-14 13:15:03 +0200

    feature/WY-223 update all search forms to use the same template, and update search styles and various lables and texts

--- searchform.php
+++ searchform.php
@@ -1,16 +1,9 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-    <label for="search-input" class="screen-reader-text">
-		<?php echo _x( 'Search for:', 'label', 'helsinki-universal' ); ?>
+    <label for="search-input">
+		<?php echo _x( 'Search by keyword', 'label', 'helsinki-universal' ); ?>
 	</label>
 	<div class="search-field hds-text-input hds-text-input__input-wrapper">
-		<input id="search-input" class="hds-text-input__input" type="search" placeholder="<?php echo esc_attr_x( 'Search…', 'placeholder', 'helsinki-universal' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-		<button class="button-reset" type="reset">
-			<span class="screen-reader-text"><?php echo esc_attr_x( 'Reset', 'reset button', 'helsinki-universal' ); ?></span>
-			<?php helsinki_svg_icon('cross-circle-fill'); ?>
-		</button>
-		<button class="button-reset" type="submit">
-			<span class="screen-reader-text"><?php echo esc_attr_x( 'Search', 'submit button', 'helsinki-universal' ); ?></span>
-			<?php helsinki_svg_icon('search'); ?>
-		</button>
+		<input id="search-input" class="hds-text-input__input" type="search" value="<?php echo get_search_query(); ?>" name="s" />
+		<button class="button hds-button" type="submit"><?php _ex( 'Search', 'search submit', 'helsinki-universal' ); ?></button>
 	</div>
 </form>
