f825eeab1f44d924abf9ec419bdd71033e7860ca
Billy Ward <billy@broomudigitals.fi>
2023-04-14 11:41:54 +0300

    feature/WY-294 add photographer metafield to media files, and include the metadata in core/image rendering

---
+++ functions/media/meta.php
@@ -0,0 +1,54 @@
+<?php
+
+function helsinki_add_image_attachment_fields_to_edit( $form_fields, $post ) {
+		
+	// Re-order the "Caption" field by removing it and re-adding it later
+	//$caption_field = $form_fields['post_excerpt'];
+	//unset($form_fields['post_excerpt']);
+	
+	// Re-order the "File URL" field
+	//$image_url_field = $form_fields['image_url'];
+	//unset($form_fields['image_url']);
+	
+	// Add Caption before Credit field 
+	//$form_fields['post_excerpt'] = $caption_field;
+	
+	// Add a Photographer field
+	$form_fields["photographer_text"] = array(
+		"label" => __("Photographer", "helsinki-universal"),
+		"input" => "text",
+		"value" => esc_attr( get_post_meta($post->ID, "_photographer_text", true) ),
+	);
+		
+	// Add Caption before Credit field 
+	//$form_fields['image_url'] = $image_url_field;
+	
+	return $form_fields;
+}
+add_filter("attachment_fields_to_edit", "helsinki_add_image_attachment_fields_to_edit", null, 2);
+
+function helsinki_add_image_attachment_fields_to_save( $post, $attachment ) {
+    var_dump($attachment);
+	if ( isset( $attachment['photographer_text'] ) ) {
+		update_post_meta( $post['ID'], '_photographer_text', esc_attr($attachment['photographer_text']) );
+
+        if (function_exists('pll_register_string')) {
+            $id = $post['ID'];
+            pll_register_string( "photographer-text-{$id}", $attachment['photographer_text'], 'Helsinki Universal', false );
+        }	
+    }
+	return $post;
+}
+add_filter("attachment_fields_to_save", "helsinki_add_image_attachment_fields_to_save", null , 2);
+
+function helsinki_base_image_credit( $post_ID = null ) {
+
+    $attachment_fields = get_post_custom( $post_ID );
+
+    $credit_text = ( isset($attachment_fields['_photographer_text'][0]) && !empty($attachment_fields['_photographer_text'][0]) ) ? esc_attr($attachment_fields['_photographer_text'][0]) : '';
+    
+    $credit = ( $credit_text ) ? sprintf(__("Photo: %s", 'helsinki-universal'), $credit_text) : false;
+
+    return $credit;
+
+}
