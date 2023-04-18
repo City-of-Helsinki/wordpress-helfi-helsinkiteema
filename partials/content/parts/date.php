9a5430adc3798f1298b6d58cbf306f3bbb0a4aaf
Billy Ward <billy@broomudigitals.fi>
2023-04-18 14:15:55 +0300

    feature/WY-401 fix minutes in get_the_date functions

--- partials/content/parts/date.php
+++ partials/content/parts/date.php
@@ -1,6 +1,6 @@
 <span class="content__date date">
 	<span class="screen-reader-text"><?php esc_html_e('Published', 'helsinki-universal'); ?>:</span>
 	<time datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>">
-		<?php echo get_the_date('d.m.Y H:m'); ?>
+		<?php echo get_the_date('d.m.Y H:i'); ?>
 	</time>
 </span>
