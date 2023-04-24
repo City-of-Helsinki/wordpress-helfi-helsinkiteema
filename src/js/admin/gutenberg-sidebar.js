1e699e1d9601b294a2e071910f57a41dbbf75fa4
Billy Ward <billy@broomudigitals.fi>
2023-04-24 11:47:41 +0300

    feature/WY-22 clean comments

--- src/js/admin/gutenberg-sidebar.js
+++ src/js/admin/gutenberg-sidebar.js
@@ -1,11 +1,5 @@
 (function( wp ) {
 
-    //write a script that hides/shows an element based on the current page template
-    //if the current page template is default then show the element
-    //if the current page template is not default then hide the element
-    //element visibility should be toggled on page template change
-    //element visibility should be toggled on page load
-
     //get the current page template
     var currentTemplate = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'template' );
 
