b4d87717c141fc38128f37fa3299c5767baac018
Billy Ward <billy@broomudigitals.fi>
2023-05-12 09:28:02 +0300

    feature/WY-481 update complianz banner aria-labelledby to reference banner heading, and aria-describedby to reference the description

--- complianz-gdpr-premium/templates/cookiebanner.php
+++ complianz-gdpr-premium/templates/cookiebanner.php
@@ -14,8 +14,8 @@
 <!-- Header - Including Logo, Title, Close Icon -->
 
 <div class="cmplz-cookiebanner cmplz-hidden banner-{id} {consent_type} cmplz-{position} cmplz-categories-type-{use_categories}" aria-modal="true" data-nosnippet="true" role="dialog" aria-live="polite" aria-labelledby="cmplz-header-{id}-{consent_type}" aria-describedby="cmplz-message-{id}-{consent_type}">
-<div class="cmplz-title screen-reader-text" id="cmplz-header-{id}-{consent_type}">{header}</div>
 	<!--<div class="cmplz-header">
+		<div class="cmplz-title screen-reader-text" id="cmplz-header-{id}-{consent_type}">{header}</div>
 		<div class="cmplz-logo">{logo}</div>
 		<a class="cmplz-close" tabindex="0" role="button">
 			<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
@@ -28,16 +28,16 @@
 
 <!-- Body -->
 	<div class="cmplz-body">
-		<div class="cmplz-message" id="cmplz-message-{id}-{consent_type}">
+		<div class="cmplz-message">
 			<div class="cookie-notice">
-				<h2 class="cookie-notice__title">
+				<h2 class="cookie-notice__title" id="cmplz-header-{id}-{consent_type}">
 					<?php echo sprintf(
 						'%s %s',
 						get_bloginfo( 'name' ),
 						esc_html_x('uses cookies', 'cookie notice title prefix', 'helsinki-universal')
 					); ?>
 				</h2>
-				<p><?php _e('This website uses required cookies to ensure the basic functionality and performance. In addition, we use targeting cookies to improve the user experience, perform analytics and display personalised content.', 'helsinki-universal') ?></p>
+				<p id="cmplz-message-{id}-{consent_type}"><?php _e('This website uses required cookies to ensure the basic functionality and performance. In addition, we use targeting cookies to improve the user experience, perform analytics and display personalised content.', 'helsinki-universal') ?></p>
 			</div>
 		</div>
 		
