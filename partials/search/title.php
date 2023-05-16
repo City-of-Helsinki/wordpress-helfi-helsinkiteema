5c2e6371bd83ea1b6f73c36a0c02f2a7549d65ba
Billy Ward <billy@broomudigitals.fi>
2023-05-16 13:03:38 +0300

    fix incorrect string in search title

--- partials/search/title.php
+++ partials/search/title.php
@@ -1,6 +1,6 @@
 <div class="hds-container">
 	<h2 class="search-results-title view-subtitle">
-		<?php echo esc_html__('Search results', 'helsinki-universal'); ?>
+		<?php echo esc_html__('Results for keyword', 'helsinki-universal'); ?>
 		<?php if(get_search_query()) {
 			echo 	' "' . get_search_query() . '"';
 		} ?>
