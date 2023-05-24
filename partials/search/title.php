bc643b08632bd08b4f2a2a89e3f265bdd541fbf4
Billy Ward <96112727+BroomuBilly@users.noreply.github.com>
2023-05-24 08:22:56 +0300

    feature/WY-485 Remove italic from embed caption and double quotes on empty search (#145)
    Authored-by: Tommi Sipiläinen <tommi@broomudigitals.fi>

--- partials/search/title.php
+++ partials/search/title.php
@@ -1,11 +1,8 @@
 <div class="hds-container">
 	<h2 class="search-results-title view-subtitle">
-		<?php
-			printf(
-				'%s "%s"',
-				esc_html__('Search results', 'helsinki-universal'),
-				get_search_query()
-			);
-		?>
+		<?php echo esc_html__('Search results', 'helsinki-universal'); ?>
+		<?php if(get_search_query()) {
+			echo 	' "' . get_search_query() . '"';
+		} ?>
 	</h2>
 </div>
