9da90cac3c2da96b4911a26f786fc4d0ee6f8dcf
Billy Ward <billy@broomudigitals.fi>
2023-05-30 14:41:05 +0300

    feature/WY-438 update recent posts styles, add option to force placeholder image for entries, fix text cutting off in the editor in flex components

--- partials/loop/entry-grid.php
+++ partials/loop/entry-grid.php
@@ -1,7 +1,7 @@
 <div class="grid__column">
 	<article id="post-<?php the_ID(); ?>" class="<?php helsinki_entry_classes( 'grid entry--' . get_post_type() ); ?>">
 		<div>
-			<?php helsinki_entry_image(); ?>
+			<?php helsinki_entry_image(null, helsinki_entry_has_placeholder_class($args)); ?>
 			<a class="entry__link" href="<?php the_permalink(); ?>"><h2 class="entry__title"><?php helsinki_entry_title(); ?></h2></a>
 			<div class="entry__meta meta">
 				<time class="date" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>">
