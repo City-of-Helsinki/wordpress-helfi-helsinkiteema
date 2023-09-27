b7994876d212f074cef433b92a2028a681e460e5
Tommi <tommi@broomudigitals.fi>
2023-09-27 11:41:22 +0300

    Feat/WY-301: Implements Sidebar navigation  (#188)
    * WY-301 Implement sidebar-navigation
    
    * WY-301 Adjust styles
    
    * WY-301 Update language strings, clean up code
    
    * WY-301 Add menu depth to sidebar
    
    * WY-301 Translations
    
    ---------
    
    Co-authored-by: Billy Ward <96112727+BroomuBilly@users.noreply.github.com>

--- page.php
+++ page.php
@@ -4,43 +4,41 @@ get_header();
 ob_start();
 
 if (get_option('page_for_posts') == get_the_ID()) {
-  global $wp_query; 
+  global $wp_query;
   global $paged;
-  $wp_query = new WP_Query(array( 'cat' => '', 'paged' => $paged));
-	get_template_part(
-		'partials/front-page/archives',
-		null,
-		array()
-	);
+  $wp_query = new WP_Query(array('cat' => '', 'paged' => $paged));
+  get_template_part(
+    'partials/front-page/archives',
+    null,
+    array()
+  );
   wp_reset_query();
-}
-else {
+} else {
   /**
-    * Hook: helsinki_content_before
-    *
-    */
+   * Hook: helsinki_content_before
+   *
+   */
   do_action("helsinki_content_before");
 
-  while ( have_posts() ) {
+  while (have_posts()) {
 
     the_post();
 
     /**
-        * Hook: helsinki_content
-        *
-        */
-      do_action("helsinki_content");
-
+     * Hook: helsinki_content
+     *
+     */
+    do_action("helsinki_content");
   }
 
   /**
-    * Hook: helsinki_content_after
-    *
-    */
+   * Hook: helsinki_content_after
+   *
+   */
   do_action("helsinki_content_after");
 }
 
 $content = ob_get_clean();
-echo apply_filters( 'helsinki_content_output', $content);
+echo apply_filters('helsinki_content_output', $content);
 
 get_footer();
--- sidebar.php
+++ sidebar.php
@@ -1,25 +1,25 @@
 <aside class="content__sidebar">
   <?php
 
-    $post_type = get_post_type();
-    $post_id = get_the_id();
+  $post_type = get_post_type();
+  $post_id = get_the_id();
 
-    ob_start();
+  ob_start();
 
-    /**
-      * Hook: helsinki_sidebar
-      *
-      */
-    do_action('helsinki_sidebar', "sidebar-{$post_type}", $post_id);
+  /**
+   * Hook: helsinki_sidebar
+   *
+   */
+  do_action('helsinki_sidebar', "sidebar-{$post_type}", $post_id);
 
-    /**
-      * Hook: helsinki_{$post_type}_sidebar
-      *
-      */
-    do_action("helsinki_{$post_type}_sidebar", $post_id);
+  /**
+   * Hook: helsinki_{$post_type}_sidebar
+   *
+   */
+  do_action("helsinki_{$post_type}_sidebar", $post_id);
 
-    $content = ob_get_clean();
-    echo apply_filters( 'helsinki_sidebar_output', $content);    
+  $content = ob_get_clean();
+  echo apply_filters('helsinki_sidebar_output', $content);
 
   ?>
 </aside>
