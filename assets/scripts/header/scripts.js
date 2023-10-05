3a4c5ebca3aef21891a21f8a47a94a6c2de540ee
Billy Ward <billy@broomudigitals.fi>
2023-10-05 14:28:07 +0300

    bugfix/WY-583 move notification scripts to right after the notifications, and update them to use localStorage

--- assets/scripts/header/scripts.js
+++ assets/scripts/header/scripts.js
@@ -1,3 +1,82 @@
+function setFocus(element) {
+  try {
+    element.focus();
+  }
+    catch (error) {
+  }
+  return (document.activeElement === element);
+}
+
+function firstFocusableChild(element) {
+  var firstMenuItem = element.querySelector('.menu li a');
+  if ( firstMenuItem ) {
+    return firstMenuItem;
+  }
+  var firstInput = element.querySelector('input');
+  if ( firstInput ) {
+    return firstInput;
+  }
+  return element.firstElementChild;
+}
+
+function lastFocusableChild(element) {
+  return element.lastElementChild;
+}
+
+function focusFirstChild(element) {
+  for (var i = 0; i < element.childNodes.length; i++) {
+    if ( setFocus(element.childNodes[i]) || focusFirstChild(element.childNodes[i]) ) {
+      return true;
+    }
+  }
+  return null;
+}
+
+function focusLastChild(element) {
+  for (var i = element.childNodes.length - 1; i >= 0; i--) {
+    if ( setFocus(element.childNodes[i]) || focusLastChild(element.childNodes[i]) ) {
+      return true;
+    }
+  }
+  return null;
+}
+
+function focusToPrevious( element ) {
+  var _previousFocusable;
+  if ( element.previousElementSibling !== null ) { 
+    _previousFocusable = element.previousElementSibling;
+  }
+  else {
+    var _parent = element.parentElement;
+    while ( _parent ) {
+      if ( _parent.previousElementSibling !== null ) {
+        _previousFocusable = _parent.previousElementSibling;
+        break;
+      }
+      _parent = _parent.parentElement;
+    }
+  }
+
+  while ( _previousFocusable ) {
+    if ( focusLastChild(_previousFocusable) === true ) {
+      break;
+    }
+    if ( _previousFocusable.previousElementSibling !== null ) {
+      _previousFocusable = _previousFocusable.previousElementSibling;
+    }
+    else {
+      var _parent = _previousFocusable.parentElement;
+      while ( _parent ) {
+        if ( _parent.previousElementSibling !== null ) {
+          _previousFocusable = _parent.previousElementSibling;
+          break;
+        }
+        _parent = _parent.parentElement;
+      }
+    }
+  }
+}
+
 Hyphenopoly.config({
     require: {
         "fi": "lentokonesuihkuturbiinimoottoriapumekaanikkoaliupseerioppilas",
