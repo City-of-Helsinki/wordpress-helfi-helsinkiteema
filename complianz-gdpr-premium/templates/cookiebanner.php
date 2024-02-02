0356226342c076cfeb6da117c7e7621d0170a8b6
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-02-02 11:33:40 +0200

    Update cookiebanner.php

--- complianz-gdpr-premium/templates/cookiebanner.php
+++ complianz-gdpr-premium/templates/cookiebanner.php
@@ -10,12 +10,21 @@
 	$unique = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(10/strlen($x)) )),1,10);;
 
 ?>
+<!-- Header - Including Logo, Title, Close Icon -->
 <div class="cmplz-cookiebanner cmplz-hidden banner-{id} {consent_type} cmplz-{position} cmplz-categories-type-{use_categories}" aria-modal="true" data-nosnippet="true" role="dialog" aria-live="polite" aria-labelledby="cmplz-header-{id}-{consent_type}" aria-describedby="cmplz-message-{id}-{consent_type}">
+	<!--<div class="cmplz-header">
+		<div class="cmplz-title screen-reader-text" id="cmplz-header-{id}-{consent_type}">{header}</div>
+		<div class="cmplz-logo">{logo}</div>
+		<a class="cmplz-close" tabindex="0" role="button">
+			<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+		</a>
+	</div>-->
 
-	<div class="cmplz-cookiebanner__container">
+	<!-- Divider between Header and Body -->
 
-		<div class="cmplz-divider cmplz-divider-header"></div>
+	<div class="cmplz-divider cmplz-divider-header"></div>
 
+	<!-- Body -->
 		<div class="cmplz-body">
 			<div class="cmplz-message">
 				<div class="cookie-notice">
@@ -30,8 +39,9 @@
 				</div>
 			</div>
 
-
 		</div>
+
+		<!-- Body - Hyperlinks for TCF -->
 
 		<div class="cmplz-links cmplz-information">
 			<a class="cmplz-link cmplz-manage-options cookie-statement" href="#" data-relative_url="#cmplz-manage-consent-container"><?php _e("Manage options","complianz-gdpr")?></a>
@@ -41,8 +51,12 @@
 			<?php do_action("cmplz_after_links")?>
 		</div>
 
+		<!-- Footer -->
+		<!-- Footer - Divider -->
+
 		<div class="cmplz-divider cmplz-footer"></div>
 
+		<!-- Footer - Hyperlinks Legal Documents -->
 		<div class="cmplz-links cmplz-documents">
 			<a class="cmplz-link cookie-statement" href="#" data-relative_url="">{title}</a>
 			<a class="cmplz-link privacy-statement" href="#" data-relative_url="">{title}</a>
@@ -50,9 +64,13 @@
 			<?php do_action("cmplz_after_documents")?>
 		</div>
 
+		<!-- Body - Categories -->
+
+		<!-- categories start -->
 		<div class="cookie-categories-wrap">
 			<div class="cmplz-categories">
 				<div class="cmplz-categories-inner">
+					<!-- Body - Categories - Functional -->
 					<div class="cmplz-categories-wrap">
 						<label class="cmplz-category cmplz-functional" for="cmplz-functional-{consent_type}-<?php echo $unique; ?>">
 							<div class="cmplz-slider-checkbox">
@@ -70,6 +88,7 @@
 						</label>
 					</div>
 
+					<!-- Body - Categories - Preferences -->
 					<div class="cmplz-categories-wrap">
 						<label class="cmplz-category cmplz-preferences" for="cmplz-preferences-{consent_type}-<?php echo $unique; ?>">
 							<div class="cmplz-slider-checkbox">
@@ -85,6 +104,7 @@
 						</label>
 					</div>
 
+					<!-- Body - Categories - Statistics -->
 					<div class="cmplz-categories-wrap">
 						<label class="cmplz-category cmplz-statistics" for="cmplz-statistics-{consent_type}-<?php echo $unique; ?>">
 							<div class="cmplz-slider-checkbox">
@@ -100,6 +120,7 @@
 						</label>
 					</div>
 
+					<!-- Body - Categories - Marketing -->
 					<div class="cmplz-categories-wrap">
 						<label class="cmplz-category cmplz-marketing" for="cmplz-marketing-{consent_type}-<?php echo $unique; ?>">
 							<div class="cmplz-slider-checkbox">
@@ -116,9 +137,11 @@
 					</div>
 				</div>
 			</div>
-		</div>
+		</div><!-- categories end -->
 
 		<?php do_action('cmplz_banner_after_categories' ) ?>
+
+		<!-- Footer - Buttons -->
 
 		<div class="cmplz-buttons">
 			<button class="cmplz-btn cmplz-accept"><?php _e('Accept all cookies', 'helsinki-universal') ?></button>
