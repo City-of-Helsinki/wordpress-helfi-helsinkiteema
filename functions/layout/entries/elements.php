b65e797f26c51dc340c873ee954a0e562d2bfb5b
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-10-03 13:03:16 +0300

    Update elements.php

--- functions/layout/entries/elements.php
+++ functions/layout/entries/elements.php
@@ -4,6 +4,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
+/**
+  * Title
+  */
+if ( ! function_exists('helsinki_entry_title') ) {
+	function helsinki_entry_title(): void {
+		echo helsinki_get_entry_title();
+	}
+}
+
+if ( ! function_exists('helsinki_get_entry_title') ) {
+	function helsinki_get_entry_title(): string {
+		return get_the_title();
+	}
+}
+
 function helsinki_entry_title_with_link( WP_Post $post = null ): string {
 	$level = helsinki_entry_title_heading_level( $post );
 
@@ -34,6 +49,9 @@ function helsinki_entry_link_with_title( WP_Post $post = null ): string {
 	return apply_filters( 'helsinki_entry_link_with_title_html', $html, $level, $post );
 }
 
+/**
+  * Excerpt
+  */
 function helsinki_entry_excerpt( WP_Post $post = null ): string {
 	$html = sprintf(
 		'<div class="entry__excerpt excerpt size-l">
@@ -45,6 +63,9 @@ function helsinki_entry_excerpt( WP_Post $post = null ): string {
 	return apply_filters( 'helsinki_entry_excerpt_html', $html, $post );
 }
 
+/**
+  * Date
+  */
 function helsinki_entry_published_date( WP_Post $post = null ): string {
 	$html = sprintf(
 		'<time class="date" datetime="%1$s">
@@ -58,6 +79,9 @@ function helsinki_entry_published_date( WP_Post $post = null ): string {
 	return apply_filters( 'helsinki_entry_published_date_html', $html, $post );
 }
 
+/**
+  * Categories
+  */
 function helsinki_entry_categories( WP_Post $post = null ): string {
 	$html = sprintf(
 		'<span class="content__category categories">
@@ -68,4 +92,67 @@ function helsinki_entry_categories( WP_Post $post = null ): string {
 	);
 
 	return apply_filters( 'helsinki_entry_categories_html', $html, $post );
+}
+
+/**
+  * Thumbnail
+  */
+function helsinki_entry_image_icon(): string {
+	return helsinki_get_svg_placeholder(
+		apply_filters('helsinki_entry_image_icon', helsinki_entry_image_icon_name())
+	);
+}
+
+function helsinki_entry_image( $post = null, $force_placeholder = false ): void {
+	echo helsinki_get_entry_image( $post, $force_placeholder );
+}
+
+function helsinki_get_entry_image( $post = null, $force_placeholder = false ): string {
+	$image = helsinki_get_entry_image_html(
+		$post ? $post : get_post(),
+		helsinki_entry_image_size(),
+		array()
+	);
+
+	if ( ! $image || $force_placeholder ) {
+		return helsinki_get_entry_image_with_wrap(
+			helsinki_entry_image_icon(),
+			helsinki_entry_image_classes(true)
+		);
+	} else {
+		return helsinki_get_entry_image_with_wrap(
+			$image,
+			helsinki_entry_image_classes()
+		);
+	}
+}
+
+function helsinki_get_entry_image_with_wrap( string $image, string $class ): string {
+	return sprintf(
+		'<div class="%s">%s</div>',
+		$class,
+		helsinki_image_with_wrap( $image, true )
+	);
+}
+
+function helsinki_get_entry_image_html( WP_Post $post, string $size, array $attr = array() ): string {
+	return has_post_thumbnail( $post ) ? get_the_post_thumbnail($post, $size, $attr) : helsinki_get_entry_default_image($size, $attr);
+}
+
+if ( ! function_exists('helsinki_entry_default_image') ) {
+	function helsinki_entry_default_image( string $size = 'post-thumnbnail', array $attr = array(), int $post_id = 0 ): void {
+		if ( ! $post_id ) {
+			$post_id = get_the_ID();
+		}
+
+		echo helsinki_get_entry_default_image($size, $attr, $post_id);
+	}
+}
+
+if ( ! function_exists('helsinki_get_entry_default_image') ) {
+	function helsinki_get_entry_default_image( string $size = 'post-thumnbnail', array $attr = array(), int $post_id = 0 ): string {
+		$img_id = helsinki_entry_default_image_id( $post_id ?: get_the_ID() );
+
+		return $img_id ? wp_get_attachment_image( $img_id, $size, false, $attr ): '';
+	}
 }
