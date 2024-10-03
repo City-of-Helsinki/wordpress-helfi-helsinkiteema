5c33eda0b45de0214b97e84b4d69a2a8125a8b15
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-10-03 12:11:41 +0300

    Update entry-grid.php

--- partials/loop/entry-grid.php
+++ partials/loop/entry-grid.php
@@ -1,17 +1,23 @@
 <div class="grid__column">
 	<article id="post-<?php the_ID(); ?>" class="<?php helsinki_entry_classes( 'grid entry--' . get_post_type() ); ?>">
+
+		<?php do_action( 'helsinki_entry_content_before', get_post(), 'grid', $args ); ?>
+
 		<div>
-			<?php helsinki_entry_image(null, helsinki_entry_has_placeholder_class($args)); ?>
-			<a class="entry__link" href="<?php the_permalink(); ?>"><h2 class="entry__title"><?php helsinki_entry_title(); ?></h2></a>
+			<?php do_action( 'helsinki_entry_content', get_post(), 'grid', $args ); ?>
+
 			<div class="entry__meta meta">
-				<time class="date" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>">
-					<span class="screen-reader-text"><?php esc_html_e( 'Published:', 'helsinki-universal' ); ?></span>
-					<?php echo get_the_date(); ?>
-				</time>
+
+				<?php do_action( 'helsinki_entry_meta', get_post(), 'grid', $args ); ?>
+
 			</div>
+
 			<div class="entry__more">
 				<?php helsinki_svg_icon('arrow-right'); ?>
 			</div>
 		</div>
+
+		<?php do_action( 'helsinki_entry_content_after', get_post(), 'grid', $args ); ?>
+
 	</article>
 </div>
