9e9fd586660a390ab02c460c099d5f33cb97c5b4
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-01-10 14:57:55 +0200

    Update menu.php

--- partials/footer/menu.php
+++ partials/footer/menu.php
@@ -1,2 +1,11 @@
 <?php
-echo helsinki_menu('footer_menu');
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+<nav id="footer-menu-nav" class="footer__navigation" aria-labelledby="footer-menu-nav-label">
+	<span id="footer-menu-nav-label" class="screen-reader-text">
+		<?php echo esc_html_x( 'Quick links', 'Footer menu nav label', 'helsinki-universal' ); ?>
+	</span>
+	<?php echo helsinki_menu( 'footer_menu' ); ?>
+</nav>
