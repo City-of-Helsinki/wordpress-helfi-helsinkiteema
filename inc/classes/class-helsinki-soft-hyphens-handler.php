ea89d8f1054fad6d022ce56da83c0dda5a715821
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-01-15 14:34:37 +0200

    Update class-helsinki-soft-hyphens-handler.php

--- inc/classes/class-helsinki-soft-hyphens-handler.php
+++ inc/classes/class-helsinki-soft-hyphens-handler.php
@@ -6,18 +6,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Helsinki_Soft_Hyphens_Handler
 {
-	public function handle( string $content ): string
-	{
-		return $this->aria_hide_soft_hyphens( $content );
-	}
-
-	protected function aria_hide_soft_hyphens( string $content ): string
+	public function aria_hide_soft_hyphens( string $content ): string
 	{
 		return preg_replace(
 			$this->soft_hyphens_pattern(),
 			$this->aria_hidden_soft_hyphen(),
 			$content
 			) ?: $content;
+	}
+
+	public function remove_soft_hyphens( string $content ): string
+	{
+		return preg_replace( $this->soft_hyphens_pattern(), '', $content ) ?: $content;
 	}
 
 	protected function soft_hyphens_pattern(): string
