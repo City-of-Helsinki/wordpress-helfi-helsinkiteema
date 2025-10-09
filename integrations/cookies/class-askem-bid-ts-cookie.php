01ce46a39441c0f149eda99e4604f2a072dd55bf
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-10-09 17:00:49 +0300

    Create class-askem-bid-ts-cookie.php

---
+++ integrations/cookies/class-askem-bid-ts-cookie.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace CityOfHelsinki\WordPress\Helsinki\Theme\Integrations\Cookies;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+use CityOfHelsinki\WordPress\CookieConsent\Features\Interfaces\Known_Cookie_Data;
+
+final class Askem_Bid_Ts_Cookie implements Known_Cookie_Data
+{
+	public function issuer(): string
+	{
+		return 'Askem';
+	}
+
+	public function name(): string
+	{
+		return 'rnsbid_ts';
+	}
+
+	public function label(): string
+	{
+		return 'rnsbid_ts';
+	}
+
+	public function descriptionTranslations(): array
+	{
+		return array(
+			'fi' => 'Askem-reaktionappien toimintaan liittyvä tietue.',
+			'sv' => 'En post relaterad till driften av reaktionsknappen Askem.',
+			'en' => 'A record related to the operation of the Askem react buttons.'
+		);
+	}
+
+	public function retentionTranslations(): array
+	{
+		return array(
+			'fi' => '-',
+			'sv' => '-',
+			'en' => '-'
+		);
+	}
+
+	public function type(): string
+	{
+		return 'localstorage';
+	}
+
+	public function category(): string
+	{
+		return 'statistics';
+	}
+}
