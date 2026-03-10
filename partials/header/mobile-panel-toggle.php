1e830e1563141563282accedacaee63b73e1137c
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-03-10 15:16:00 +0200

    single text in menu toggle

--- partials/header/mobile-panel-toggle.php
+++ partials/header/mobile-panel-toggle.php
@@ -8,20 +8,11 @@
 	data-no-scroll-limit="down">
 	<span class="js-toggle__open">
 		<?php helsinki_svg_icon( 'menu-hamburger' ); ?>
-		<span class="text" aria-hidden="true">
-			<?php echo esc_html_x( 'Menu', 'Label', 'helsinki-universal' ); ?>
-		</span>
-		<span class="screen-reader-text">
-			<?php echo esc_html_x( 'Open the navigation menu', 'Label - Toggle - Mobile menu', 'helsinki-universal' ); ?>
-		</span>
 	</span>
 	<span class="js-toggle__close">
 		<?php helsinki_svg_icon( 'cross' ); ?>
-		<span class="text" aria-hidden="true">
-			<?php echo esc_html_x( 'Close', 'verb', 'helsinki-universal' ); ?>
-		</span>
-		<span class="screen-reader-text">
-			<?php echo esc_html_x( 'Close the navigation menu', 'Label - Toggle - Mobile menu', 'helsinki-universal' ); ?>
-		</span>
+	</span>
+	<span class="text">
+		<?php echo esc_html_x( 'Menu', 'Label', 'helsinki-universal' ); ?>
 	</span>
 </button>
