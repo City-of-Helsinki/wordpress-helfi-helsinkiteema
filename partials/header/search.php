6c0cfa771cb58684702bd225169107732e63798c
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-03-16 12:15:35 +0200

    Update search.php

--- partials/header/search.php
+++ partials/header/search.php
@@ -1,4 +1,4 @@
-<div class="navigation__search">
+<div class="nav-toggle nav-toggle--search">
 	<button
 		id="header-search-toggle"
 		class="button-reset has-icon has-icon--above js-toggle js-toggle-no-scroll"
@@ -17,11 +17,11 @@
 			<?php echo esc_html_x( 'Search', 'search toggle', 'helsinki-universal' ); ?>
 		</span>
 	</button>
-	<div id="header-search" aria-labelledby="header-search-toggle" role="region">
-		<div class="hds-container">
-			<h2 class="search-title"><?php echo esc_html_x('Search the site', 'search title', 'helsinki-universal'); ?></h2>
+</div>
+<div id="header-search" aria-labelledby="header-search-toggle" role="region" hidden>
+	<div class="hds-container">
+		<h2 class="search-title"><?php echo esc_html_x('Search the site', 'search title', 'helsinki-universal'); ?></h2>
 
-			<?php do_action( 'helsinki_search_form', 'header' ); ?>
-		</div>
+		<?php do_action( 'helsinki_search_form', 'header' ); ?>
 	</div>
 </div>
