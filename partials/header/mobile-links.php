085f8dabc2a918065121e2cd7e119a2f7320aa8d
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-03-16 12:15:27 +0200

    Update mobile-links.php

--- partials/header/mobile-links.php
+++ partials/header/mobile-links.php
@@ -1,14 +1,20 @@
 <div class="mobile-links">
-	<?php if ( $args['menu'] ) { ?>
-		<?php echo $args['menu']; ?>
-	<?php } ?>
+	<?php
+		if ( $args['menu'] ) {
+			echo $args['menu'];
+		}
+	?>
 
 	<ul class="menu menu--vertical">
 		<li class="menu__item">
-			<a href="<?php echo esc_url( $args['branding']['url'] ); ?>"><?php echo esc_html( $args['branding']['title'] ); ?></a>
+			<a href="<?php echo esc_url( $args['branding']['url'] ); ?>">
+				<?php echo esc_html( $args['branding']['title'] ); ?>
+			</a>
 		</li>
 		<li class="menu__item">
-			<a href="<?php echo esc_url( $args['feedback']['url'] ); ?>"><?php echo esc_html( $args['feedback']['title'] ); ?></a>
+			<a href="<?php echo esc_url( $args['feedback']['url'] ); ?>">
+				<?php echo esc_html( $args['feedback']['title'] ); ?>
+			</a>
 		</li>
 	</ul>
 </div>
