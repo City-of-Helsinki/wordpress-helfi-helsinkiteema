1e2263cc5bcc876a9581cf86ef2dcccacaab649f
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-03-20 10:31:11 +0200

    Update form-title.php

--- partials/search/form-title.php
+++ partials/search/form-title.php
@@ -1,1 +1,3 @@
-<h1 class="view-title"><?php echo esc_html_x('Search the site', 'search title', 'helsinki-universal'); ?></h1>
+<h1 id="<?php echo esc_attr( $args['id'] ?? '' ); ?>" class="view-title">
+	<?php echo esc_html_x('Search the site', 'search title', 'helsinki-universal'); ?>
+</h1>
