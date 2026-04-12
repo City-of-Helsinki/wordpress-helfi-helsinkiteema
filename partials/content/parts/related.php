1c010ec7a824a536ac91924d764e191999b8f59f
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-12 19:46:39 +0300

    Update related.php

--- partials/content/parts/related.php
+++ partials/content/parts/related.php
@@ -3,15 +3,15 @@
 		<h2><?php esc_html_e('You might be interested in', 'helsinki-universal'); ?></h2>
 		<div class="feed-posts">
 			<ul class="posts">
-			<?php
-				global $post;
-				foreach ( $args['posts'] as $post ) {
-					setup_postdata( $post );
-					helsinki_feed_entry();
-				}
-		        wp_reset_postdata();
-			?>
-			<ul>
+				<?php
+					global $post;
+					foreach ( $args['posts'] as $post ) {
+						setup_postdata( $post );
+						helsinki_feed_entry();
+					}
+			        wp_reset_postdata();
+				?>
+			</ul>
 		</div>
 	</div>
 </aside>
