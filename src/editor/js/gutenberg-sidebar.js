d574fef5db1b46e444d4958ba6cea67dcc9a5ed5
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-13 18:04:20 +0300

    Update gutenberg-sidebar.js

--- src/editor/js/gutenberg-sidebar.js
+++ src/editor/js/gutenberg-sidebar.js
@@ -1,28 +1,19 @@
-(function( wp ) {
+(function( wp, element ) {
+  if ( wp && element ) {
 
-    //get the current page template
-    var currentTemplate = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'template' );
+    function toggleMetaboxDisplay() {
+      var currentTemplate = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'template' );
 
-    //get the element to hide/show
-    var element = document.getElementById( 'helsinki-sidebar-settings' );
-
-    if ( currentTemplate !== 'template/landing-page.php' && currentTemplate !== 'template/no-sidebar.php' ) {
+      if ( currentTemplate !== 'template/landing-page.php' && currentTemplate !== 'template/no-sidebar.php' ) {
         element.style.display = 'block';
-    }
-    else {
+      } else {
         element.style.display = 'none';
+      }
     }
+
+    toggleMetaboxDisplay();
 
     //element visibility should be toggled on page template change
-    wp.data.subscribe( function() {
-        var currentTemplate = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'template' );
-        if ( currentTemplate !== 'template/landing-page.php' && currentTemplate !== 'template/no-sidebar.php' ) {
-            element.style.display = 'block';
-        }
-        else {
-            element.style.display = 'none';
-        }
-    } );
-
-
-} )( window.wp );
+    wp.data.subscribe( () => toggleMetaboxDisplay() );
+  }
+} )( window.wp, document.getElementById( 'helsinki-sidebar-settings' ) );
