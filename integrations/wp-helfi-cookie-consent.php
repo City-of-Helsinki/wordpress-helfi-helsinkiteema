939c731c0db2dabfa67b63943f8d4eae5f856364
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-08-11 10:05:38 +0300

    provide cookie consent feedback script integration

--- integrations/wp-helfi-cookie-consent.php
+++ integrations/wp-helfi-cookie-consent.php
@@ -10,8 +10,12 @@ use CityOfHelsinki\WordPress\Helsinki\Theme\Integrations\Cookies\Askem_Bid_Cooki
 use CityOfHelsinki\WordPress\Helsinki\Theme\Integrations\Cookies\Askem_Bid_Ts_Cookie;
 use CityOfHelsinki\WordPress\Helsinki\Theme\Integrations\Cookies\Askem_Reaction_Cookie;
 use CityOfHelsinki\WordPress\Helsinki\Theme\Integrations\Cookies\Helsinki_Localstorage_Notification;
+use function CityOfHelsinki\WordPress\Helsinki\Theme\Integrations\Askem\feedback_buttons_script_url;
 
-\add_filter( 'wordpress_helfi_cookie_consent_known_cookies', __NAMESPACE__ . '\\provide_theme_cookies' );
+\add_filter(
+	'wordpress_helfi_cookie_consent_known_cookies',
+	__NAMESPACE__ . '\\provide_theme_cookies'
+);
 function provide_theme_cookies( array $cookies ): array {
 	return array_merge( $cookies, array(
 		Askem_Bid_Cookie::class,
@@ -19,4 +23,13 @@ function provide_theme_cookies( array $cookies ): array {
 		Askem_Reaction_Cookie::class,
 		Helsinki_Localstorage_Notification::class,
 	) );
+}
+
+\add_filter(
+	'wordpress_helfi_cookie_consent_script_placeholder_cookie_host',
+	__NAMESPACE__ . '\\provide_script_placeholder_cookie_host',
+	10, 2
+);
+function provide_script_placeholder_cookie_host( string $host, string $source ): string {
+	return feedback_buttons_script_url() === $source ? 'Askem' : $host;
 }
