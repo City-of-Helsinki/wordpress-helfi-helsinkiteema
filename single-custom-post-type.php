02ac5756033f5451c7e4e93fbe9fa32218041993
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-09-25 17:15:27 +0300

    Update single-custom-post-type.php

--- single-custom-post-type.php
+++ single-custom-post-type.php
@@ -4,28 +4,28 @@ get_header();
 ob_start();
 
 /**
-  * Hook: helsinki_cpt_content_before
+  * Hook: helsinki_content_before
   *
   */
-do_action( 'helsinki_cpt_content_before' );
+do_action( 'helsinki_content_before' );
 
 while ( have_posts() ) {
 
 	the_post();
 
 	/**
-      * Hook: helsinki_cpt_content
+      * Hook: helsinki_content
       *
       */
-    do_action( 'helsinki_cpt_content' );
+    do_action( 'helsinki_content' );
 
 }
 
 /**
-  * Hook: helsinki_cpt_content_after
+  * Hook: helsinki_content_after
   *
   */
-do_action( 'helsinki_cpt_content_after' );
+do_action( 'helsinki_content_after' );
 
 echo apply_filters( 'helsinki_content_output', ob_get_clean() );
 
